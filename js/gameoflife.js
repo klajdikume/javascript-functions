4b912eaff98bde3390466da4ef4cdbd57d6bcbdc
klajdikume <klajdi.kume@fshnstudent.info>
2021-09-17 21:27:04 +0200

    fist commit

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,14 +1,20 @@
-function seed(a,b,c) {
+function seed() {
  // returns args as array
-  return new Array(a,b,c);
+  return Array.prototype.slice.call(arguments);
 }
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  return x === j && y === k;
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  return this.some((c) => same(c, cel));
+}
 
-const printCell = (cell, state) => {};
+const printCell = (cell, state) => {
+  return contains.call(state,cell) ? "\u26A3": "\u25A2";
+};
 
 const corners = (state = []) => {};
 
